d3af57abd9db8277a330763c874b5a3227009cb2
Gui Talarico <gtalarico@gmail.com>
2017-09-20 17:21:12 -0400

    Fix setup.py requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url='https://github.com/gtalarico/airtable-python-wrapper',
     version=__version__,
     packages=['airtable'],
-    install_requires=['requests>=2.18.3'],
+    install_requires=['requests>=2.18.3', 'six>=1.10.0'],
     keywords=['airtable', 'api'],
     license='The MIT License (MIT)',
     classifiers=[
