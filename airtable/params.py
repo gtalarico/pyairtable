b64573f9290009458c4433dc54b39978bcce3f56
Gui Talarico <gtalarico@gmail.com>
2017-10-08 18:50:39 -0400

    New docs on Parameter Filter Classes

--- airtable/params.py
+++ airtable/params.py
@@ -1,5 +1,18 @@
 """
-Airtable Parameter Filters
+Parameter filters are instantiated internally
+by using the corresponding keywords.
+
+Filter names (kwargs) can be either the API camelCase name (ie ``maxRecords``)
+or the snake-case equivalent (``max_records``).
+
+Refer to the :any:`Airtable` class to verify which kwargs can be
+used with each method.
+
+The purpose of these classes is to 1. improve flexibility and
+ways in which parameter filter values can be passed, and 2. properly format
+the parameter names and values on the request url.
+
+For more information see the full implementation below.
 
 """  #
 
@@ -65,14 +78,84 @@ class _BaseObjectArrayParam(_BaseParam):
 class AirtableParams():
 
     class MaxRecordsParam(_BaseParam):
+        """
+        Max Records Param
+
+        Kwargs:
+            ``max_records=`` or ``maxRecords=``
+
+        The maximum total number of records that will be returned.
+
+        Usage:
+
+        >>> airtable.get_all(view='My View')
+
+        Args:
+            max_records (``int``): The maximum total number of records that
+                will be returned.
+
+
+        """
+
+        # Class Input > Output
+        # >>> filter = MaxRecordsParam(100)
+        # >>> filter.to_param_dict()
+        # {'maxRecords: 100}
+
         param_name = 'maxRecords'
         kwarg = 'max_records'
 
     class ViewParam(_BaseParam):
+        """
+        View Param
+
+        Kwargs:
+            ``view=``
+
+        If set, only the records in that view will be returned.
+        The records will be sorted according to the order of the view.
+
+        Usage:
+
+        >>> airtable.get_all(view='My View')
+
+        Args:
+            view (``str``): The name or ID of a view.
+
+        """
+
+        # Class Input > Output
+        # >>> filter = ViewParam('Name or Id Of View')
+        # >>> filter.to_param_dict()
+        # {'view: 'Name or Id Of View'}
+
         param_name = 'view'
         kwarg = param_name
 
     class PageSizeParam(_BaseParam):
+        """
+        Page Size Param
+
+        Kwargs:
+            ``page_size=`` or ``pageSize=``
+
+        Limits the maximum number of records returned in each request.
+        Default is 100.
+
+        Usage:
+
+        >>> airtable.get_all(page_size=50)
+
+        Args:
+            formula (``int``): The number of records returned in each request.
+                Must be less than or equal to 100. Default is 100.
+
+        """
+        # Class Input > Output
+        # >>> filter = PageSizeParam(50)
+        # >>> filter.to_param_dict()
+        # {'pageSize: 50}
+
         param_name = 'pageSize'
         kwarg = 'page_size'
 
@@ -80,17 +163,68 @@ class AirtableParams():
         """
         Formula Param
 
-        Usage:
-            >>> param = FormulaParams("FIND('XX', COL_NAME)=1")
-            >>> param.to_param_dict()
-            {'formula': "FIND('WW')=1"}
+        Kwargs:
+            ``formula=`` or ``filterByFormula=``
+
+        The formula will be evaluated for each record, and if the result
+        is not 0, false, "", NaN, [], or #Error! the record will be included
+        in the response.
+
+        If combined with view, only records in that view which satisfy the
+        formula will be returned. For example, to only include records where
+        ``COLUMN_A`` isn't empty, pass in: ``"NOT({COLUMN_A}='')"``
+
+        For more information see
+        `Airtable Docs on formulas. <https://airtable.com/api>`_
+
+        Usage - Text Column is not empty:
+
+        >>> airtable.get_all(formula="NOT({COLUMN_A}='')")
+
+        Usage - Text Column contains:
+
+        >>> airtable.get_all(formula="FIND('SomeSubText', {COLUMN_STR})=1")
+
+        Args:
+            formula (``str``): A valid Airtable formula.
 
         """
+
+        # Class Input > Output
+        # >>> param = FormulaParams("FIND('DUP', {COLUMN_STR})=1")
+        # >>> param.to_param_dict()
+        # {'formula': "FIND('WW')=1"}
+
         param_name = 'filterByFormula'
         kwarg = 'formula'
 
-    class OffsetParam(_BaseParam):
-        """ Used Internally for offset retrieving """
+    class _OffsetParam(_BaseParam):
+        """
+        Offset Param
+
+        Kwargs:
+            ``offset=``
+
+        If there are more records what was in the response,
+        the response body will contain an offset value.
+        To fetch the next page of records,
+        include offset in the next request's parameters.
+
+        This is used internally by :any:`get_all` and :any:`get_iter`.
+
+        Usage:
+
+        >>> airtable.get_iter(offset='recjAle5lryYOpMKk')
+
+        Args:
+            record_id (``str``, ``list``):
+
+        """
+        # Class Input > Output
+        # >>> filter = _OffsetParam('recqgqThAnETLuH58')
+        # >>> filter.to_param_dict()
+        # {'offset: 'recqgqThAnETLuH58'}
+
         param_name = 'offset'
         kwarg = param_name
 
@@ -98,23 +232,72 @@ class AirtableParams():
         """
         Fields Param
 
+        Kwargs:
+            ``fields=``
+
+        Only data for fields whose names are in this list will be included in
+        the records. If you don't need every field, you can use this parameter
+        to reduce the amount of data transferred.
+
         Usage:
-            >>> param = FieldsParam(['FieldOne', 'FieldTwo'])
-            >>> param.to_param_dict()
-            {'fields[]': ['FieldOne', 'FieldTwo']}
+
+        >>> airtable.get(fields='ColumnA')
+
+        Multiple Columns:
+
+        >>> airtable.get(fields=['ColumnA', 'ColumnB'])
+
+        Args:
+            fields (``str``, ``list``): Name of columns you want to retrieve.
+
         """
+
+        # Class Input > Output
+        # >>> param = FieldsParam(['FieldOne', 'FieldTwo'])
+        # >>> param.to_param_dict()
+        # {'fields[]': ['FieldOne', 'FieldTwo']}
+
         param_name = 'fields'
         kwarg = param_name
 
     class SortParam(_BaseObjectArrayParam):
         """
-        Sort Filter
+        Sort Param
+
+        Kwargs:
+            ``sort=``
+
+        Specifies how the records will be ordered. If you set the view
+        parameter, the returned records in that view will be sorted by these
+        fields.
+
+        If sorting by multiple columns, column names can be passed as a list.
+        Sorting Direction is ascending by default, but can be reversed by
+        prefixing the column name with a minus sign ``-``, or passing
+        ``COLUMN_NAME, DIRECTION`` tuples. Direction options
+        are ``asc`` and ``desc``.
 
         Usage:
-            >>> filter = SortParam([{'field': 'col', 'direction': 'asc'}])
-            >>> filter.to_param_dict()
-            {'sort[0]['field']: 'col', sort[0]['direction']: 'asc'}
+
+        >>> airtable.get(sort='ColumnA')
+
+        Multiple Columns:
+
+        >>> airtable.get(sort=['ColumnA', '-ColumnB'])
+
+        Explicit Directions:
+
+        >>> airtable.get(sort=[('ColumnA', 'asc'), ('ColumnB', 'desc')])
+
+        Args:
+            fields (``str``, ``list``): Name of columns and directions.
+
         """
+
+        # Class Input > Output
+        # >>> filter = SortParam([{'field': 'col', 'direction': 'asc'}])
+        # >>> filter.to_param_dict()
+        # {'sort[0]['field']: 'col', sort[0]['direction']: 'asc'}
 
         param_name = 'sort'
         kwarg = param_name
@@ -142,7 +325,12 @@ class AirtableParams():
 
     @classmethod
     def _discover_params(cls):
-        """ Returns a dict where filter keyword is key, and class is value """
+        """
+        Returns a dict where filter keyword is key, and class is value.
+        To handle param alias (maxRecords or max_records), both versions are
+        added.
+        """
+
         try:
             return cls.filters
         except AttributeError:
@@ -156,8 +344,8 @@ class AirtableParams():
         return cls.filters
 
     @classmethod
-    def get(cls, kwarg_name):
-        """ Returns a Param Class Instance, by its keyword name """
+    def _get(cls, kwarg_name):
+        """ Returns a Param Class Instance, by its kwarg or param name """
         param_classes = cls._discover_params()
         try:
             param_class = param_classes[kwarg_name]
