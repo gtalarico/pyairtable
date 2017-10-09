c201d36c8b4f0239a46467ca8f7c110c2075cea4
Rick Henry <fredericmhenry@gmail.com>
2017-10-09 15:11:02 -0400

    clean up test code.
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- tests/test_url_fix.py
+++ tests/test_url_fix.py
@@ -5,11 +5,15 @@ from six.moves.urllib import parse
 
 @pytest.fixture
 def db_key():
+    """A fake database key for the url."""
     return 'app0011kkk01'
 
 
 @pytest.fixture
 def table_names():
+    """Dict of some test table names with characters that are not url safe with
+    url-escaped versions.
+    """
     return {'Test table 1': 'Test%20table%201',
             'Test/Table 2': 'Test%2FTable%202',
             'Another (test) table': 'Another%20%28test%29%20table',
@@ -20,20 +24,26 @@ def table_names():
 
 @pytest.fixture
 def baseurl(db_key):
+    """The base url for the database."""
     url = parse.urljoin('https://api.airtable.com/v0/', db_key + '/')
     return url
 
 
 def _make_url(url, table_escaped):
-    # return parse.urljoin(str(url + '/'), table_escaped)
+    """Build a url correctly for testing."""
     return parse.urljoin(url, table_escaped)
 
 
 def test_url_escape(db_key, table_names, baseurl, monkeypatch):
+    """Test for proper escaping of urls including unsafe characters in table
+    names (which airtable allows).
+    """
     def rettrue(*args):
         return True
+    # It's a fake url so don't try to validate it.
     monkeypatch.setattr(Airtable, 'validate_session', rettrue)
     print('baseurl: {}'.format(baseurl))
     for name, escaped in table_names.items():
         tmp_at = Airtable(base_key=db_key, table_name=name, api_key='key1234')
-        assert str(tmp_at.url_table) == _make_url(baseurl, escaped)
+        assert str(tmp_at.url_table) == _make_url(baseurl, escaped),\
+            "Class-generated url should be properly escaped."""
