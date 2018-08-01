b3627ba8fd0f888ff403da5fe4ff8983091f37f8
gtalarico <gtalarico@gmail.com>
2018-08-01 14:44:50 -0400

    Version Bump

--- airtable/__version__.py
+++ airtable/__version__.py
@@ -1,4 +1,4 @@
-__version__ = '0.11.3'
+__version__ = '0.11.3.post.1'
 __name__ = 'airtable-python-wrapper'
 __description__ = 'Python API Wrapper for the Airtable API'
 __url__ = 'https://github.com/gtalarico/airtable-python-wrapper'
