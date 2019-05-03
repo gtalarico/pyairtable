eac6f86b28990bac868b3ca7bcd83623a0b6abb1
Brian Peterson <bpeterso2000@yahoo.com>
2019-05-02 21:26:14 -0400

    Complete airtable API CRUD tests; issue #3

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -41,6 +41,29 @@ from .test_airtable import Airtable
 Params = namedtuple('Params', ['kwds', 'params'])
 HEADER_BASE_PARAMS = {'User-Agent', 'Accept-Encoding', 'Accept', 'Connection'}
 
+TEST_DATA = {
+    "records": [
+         {
+            "id": "1",
+            "fields": {
+                "Name": "John",
+                "Phone": "2468-5790",
+                "State": "CA"
+            },
+            "createdTime": "2017-03-14T22:04:31.000Z"
+         }, {
+            "id": "2",
+            "fields": {
+                "Name": "Tom",
+                "Phone": "1234-9876",
+                "State": "FL"
+            },
+            "createdTime": "2017-03-20T15:21:50.000Z"
+         }
+    ],
+    "offset": "itr1/rec1"
+}
+
 
 def generate_responses(*args, **kwds):
     """Mocks Airtable multi-page responses.
@@ -52,30 +75,18 @@ def generate_responses(*args, **kwds):
     * Page 1 & 2 include an offset key/value along with the records.
     * Page 3 doesn't have an offset key; indicating it's the last page.
     """
-    data = {
-        "records": [
-             {
-                "id": "1",
-                "fields": {
-                    "COLUMN_ID": "1"
-                },
-                "createdTime": "2017-03-14T22:04:31.000Z"
-             }, {
-                "id": "2",
-                "fields": {
-                    "COLUMN_ID": "2"
-                },
-                "createdTime": "2017-03-20T15:21:50.000Z"
-             }
-        ],
-        "offset": "itr1/rec1"
-    }
     responses = [Mock() for i in range(3)]
     for response in responses:
         response.raise_for_status.return_value = False
-        response.data = deepcopy(data)
+        response.data = deepcopy(TEST_DATA)
     del responses[-1].data['offset']
     return responses
+
+
+def match(name, value, *args, **kwds):
+    for record in TEST_DATA['records']:
+        if record['fields'][name] == value:
+            return record
 
 
 @pytest.fixture
@@ -111,33 +122,63 @@ def airtable_pages(air_table):
 @pytest.mark.get
 @pytest.mark.parametrize('kwds,params', [
     Params(
-        kwds=dict(fields='ColumnA'),
-        params=[('offset', 'itr1/rec1')]),
+        kwds=dict(
+            fields='ColumnA'
+        ),
+        params=[
+            ('offset', 'itr1/rec1')
+        ]
+    ),
     Params(
-        kwds=dict(fields=['ColumnA', 'ColumnB']),
-        params=[('offset', 'itr1/rec1')]),
+        kwds=dict(
+            fields=['ColumnA', 'ColumnB']
+        ),
+        params=[
+            ('offset', 'itr1/rec1')
+        ]
+    ),
     Params(
-        kwds=dict(sort='ColumnA'),
-        params=[('offset', 'itr1/rec1')]),
+        kwds=dict(
+            sort='ColumnA'
+        ),
+        params=[
+            ('offset', 'itr1/rec1')
+        ]
+    ),
     Params(
-        kwds=dict(sort=['ColumnA', '-ColumnB']),
-        params=[('offset', 'itr1/rec1')]),
+        kwds=dict(
+            sort=['ColumnA', '-ColumnB']
+        ),
+        params=[
+            ('offset', 'itr1/rec1')
+        ]
+    ),
     Params(
-        kwds=dict(sort=[('ColumnA', 'asc'), ('ColumnB', 'desc')]),
-        params=[('offset', 'itr1/rec1')]),
+        kwds=dict(
+            sort=[
+                ('ColumnA', 'asc'),
+                ('ColumnB', 'desc')
+            ]
+        ),
+        params=[
+            ('offset', 'itr1/rec1')
+        ]
+    )
 ])
 def test_get_with_options(kwds, params, air_table):
     air_table.get(**kwds)
     air_table.session.request.assert_called_with(
         'get', 'https://api.airtable.com/v0/Base Key/Table%20Name',
-        json=None, params=OrderedDict(params))
+        json=None,
+        params=OrderedDict(params)
+    )
     air_table.session.request.assert_called_once()
 
 
 # --- CREATE RECORDS ---
 
 
-@pytest.mark.insert
+@pytest.mark.create
 @pytest.mark.parametrize('fields', [
     {'Name': 'Brian'},
     {'First Name': 'John'}
@@ -145,18 +186,32 @@ def test_get_with_options(kwds, params, air_table):
 def test_insert(fields, air_table):
     air_table.insert(fields)
     air_table.session.request.assert_called_with(
-        'post', 'https://api.airtable.com/v0/Base Key/Table%20Name',
-        json={'fields': fields, 'typecast': False}, params=None)
+        'post',
+        'https://api.airtable.com/v0/Base Key/Table%20Name',
+        json={
+            'fields': fields,
+            'typecast': False
+        },
+        params=None
+    )
     air_table.session.request.assert_called_once()
 
 
-@pytest.mark.batch_insert
+@pytest.mark.create
 def test_batch_insert(air_table):
     air_table.insert([{'Name': 'John'}, {'Name': 'Marc'}])
     air_table.session.request.assert_called_with(
-        'post', 'https://api.airtable.com/v0/Base Key/Table%20Name',
-        json={'fields': [{'Name': 'John'}, {'Name': 'Marc'}],
-              'typecast': False}, params=None)
+        'post',
+        'https://api.airtable.com/v0/Base Key/Table%20Name',
+        json={
+            'fields': [
+                {'Name': 'John'},
+                {'Name': 'Marc'}
+            ],
+            'typecast': False
+        },
+        params=None
+    )
     air_table.session.request.assert_called_once()
 
 
@@ -167,9 +222,17 @@ def test_mirror(airtable_pages):
     records = [{'Name': 'John'}, {'Name': 'Marc'}]
     record = airtable_pages.mirror(records)
     air_table.session.request.assert_called_with(
-        'post', 'https://api.airtable.com/v0/Base Key/Table%20Name',
-        json={'fields': [{'Name': 'John'}, {'Name': 'Marc'}],
-              'typecast': False}, params=None)
+        'post',
+        'https://api.airtable.com/v0/Base Key/Table%20Name',
+        json={
+            'fields': [
+                {'Name': 'John'},
+                {'Name': 'Marc'}
+            ],
+            'typecast': False
+        },
+        params=None
+    )
     assert airtable_pages.session.request.call_count == 2
     # record = airtable.mirror(records, view='View')
     # ([{'id': 'recwPQIfs4wKPyc9D', ... }], [{'deleted': True, ... }])
@@ -178,57 +241,117 @@ def test_mirror(airtable_pages):
 # --- READ RECORDS ---
 
 
-@pytest.mark.test_get
+@pytest.mark.read
 def test_get(air_table):
     air_table.get('recwPQIfs4wKPyc9D')
     air_table.session.request.assert_called_with(
         'get',
         'https://api.airtable.com/v0/Base Key/Table%20Name/recwPQIfs4wKPyc9D',
-        json=None, params=OrderedDict())
+        json=None,
+        params=OrderedDict()
+    )
     air_table.session.request.assert_called_once()
 
 
-@pytest.mark.get_all
+@pytest.mark.read
 @pytest.mark.parametrize('kwds,params', [
     Params(
         kwds={},
-        params=[('offset', 'itr1/rec1')]),
+        params=[
+            ('offset', 'itr1/rec1')
+        ]
+    ),
     Params(
-        kwds=dict(max_records=10),
-        params=[('maxRecords', 10), ('offset', 'itr1/rec1')]),
+        kwds=dict(
+            max_records=10
+        ),
+        params=[
+            ('maxRecords', 10),
+            ('offset', 'itr1/rec1')
+        ]
+    ),
     Params(
-        kwds=dict(view='MyView', maxRecords=20),
-        params=[('maxRecords', 20), ('offset', 'itr1/rec1'),
-                ('view', 'MyView')]),
+        kwds=dict(
+            page_size=50
+        ),
+        params=[
+            ('offset', 'itr1/rec1'),
+            ('pageSize', 50)
+        ]
+    ),
     Params(
-        kwds=dict(page_size=50),
-        params=[('offset', 'itr1/rec1'), ('pageSize', 50)]),
+        kwds=dict(
+            view='MyView',
+            maxRecords=20),
+        params=[
+            ('maxRecords', 20),
+            ('offset', 'itr1/rec1'),
+            ('view', 'MyView')
+        ]
+    ),
     Params(
         kwds=dict(view='ViewName', sort='COLUMN_A'),
-        params=[('offset', 'itr1/rec1'), ('sort[0][direction]', 'asc'),
-                ('sort[0][field]', 'COLUMN_A'), ('view', 'ViewName')]),
+        params=[
+            ('offset', 'itr1/rec1'),
+            ('sort[0][direction]', 'asc'),
+            ('sort[0][field]', 'COLUMN_A'),
+            ('view', 'ViewName')
+        ]
+    ),
     Params(
-        kwds=dict(formula="FIND('DUP', {COLUMN_STR})=1"),
-        params=[('filterByFormula', "FIND('DUP', {COLUMN_STR})=1"),
-                ('offset', 'itr1/rec1')]),
+        kwds=dict(
+            formula="FIND('DUP', {COLUMN_STR})=1"
+        ),
+        params=[
+            ('filterByFormula', "FIND('DUP', {COLUMN_STR})=1"),
+            ('offset', 'itr1/rec1')
+        ]
+    ),
     Params(
-        kwds=dict(formula="FIND('SomeSubText', {COLUMN_STR})=1"),
-        params=[('filterByFormula', "FIND('SomeSubText', {COLUMN_STR})=1"),
-                ('offset', 'itr1/rec1')]),
+        kwds=dict(
+            formula="FIND('SomeSubText', {COLUMN_STR})=1"
+        ),
+        params=[
+            ('filterByFormula', "FIND('SomeSubText', {COLUMN_STR})=1"),
+            ('offset', 'itr1/rec1')
+        ]
+    ),
     Params(
-        kwds=dict(formula="NOT({COLUMN_A}='')"),
-        params=[('filterByFormula', "NOT({COLUMN_A}='')"),
-                ('offset', 'itr1/rec1')]),
+        kwds=dict(
+            formula="NOT({COLUMN_A}='')"
+        ),
+        params=[
+            ('filterByFormula', "NOT({COLUMN_A}='')"),
+            ('offset', 'itr1/rec1')
+        ]
+    ),
     Params(
-        kwds=dict(view='MyView', fields=['ColA', '-ColB']),
-        params=[('fields[]', ['ColA', '-ColB']), ('offset', 'itr1/rec1'),
-                ('view', 'MyView')]),
+        kwds=dict(
+            view='MyView',
+            fields=['ColA', '-ColB']
+        ),
+        params=[
+            ('fields[]', ['ColA', '-ColB']),
+            ('offset', 'itr1/rec1'),
+            ('view', 'MyView')
+        ]
+    ),
     Params(
-        kwds=dict(view='MyView', fields=['ColA', '-ColB']),
-        params=[('fields[]', ['ColA', '-ColB']), ('offset', 'itr1/rec1'),
-                ('view', 'MyView')]),
+        kwds=dict(
+            view='MyView',
+            fields=['ColA', '-ColB']
+        ),
+        params=[
+            ('fields[]', ['ColA', '-ColB']),
+            ('offset', 'itr1/rec1'),
+            ('view', 'MyView')
+        ]
+    ),
     Params(
-        kwds=dict(maxRecords=3, view='All'),
+        kwds=dict(
+            maxRecords=3,
+            view='All'
+        ),
         params=[('maxRecords', 3), ('offset', 'itr1/rec1'),
                 ('view', 'All')]),
 ])
@@ -236,12 +359,14 @@ def test_get_all(kwds, params, airtable_pages):
     records = airtable_pages.get_all(**kwds)
     airtable_pages.session.request.assert_called_with(
         'get', 'https://api.airtable.com/v0/Base Key/Table%20Name',
-        json=None, params=OrderedDict(params))
+        json=None,
+        params=OrderedDict(params)
+    )
     assert airtable_pages.session.request.call_count == 3
     assert len(records) == 6
 
 
-@pytest.mark.get_all
+@pytest.mark.read
 @pytest.mark.parametrize('rate', [0, 0.2])
 def test_get_all_rate_limit(rate, airtable_pages):
     Airtable.API_LIMIT = rate
@@ -252,16 +377,20 @@ def test_get_all_rate_limit(rate, airtable_pages):
     Airtable.API_LIMIT = 0
 
 
-@pytest.mark.test_get_iter
+@pytest.mark.read
 def test_get_iter(airtable_pages):
     for page in airtable_pages.get_iter(view='ViewName', sort='COLUMN_A'):
         for num, record in enumerate(page, 1):
-            assert record['fields']['COLUMN_ID'] == str(num)
+            assert 'Name' in record['fields']
     airtable_pages.session.request.assert_called_with(
         'get', 'https://api.airtable.com/v0/Base Key/Table%20Name',
         json=None, params=OrderedDict([
-            ('offset', 'itr1/rec1'), ('sort[0][direction]', 'asc'),
-            ('sort[0][field]', 'COLUMN_A'), ('view', 'ViewName')]))
+            ('offset', 'itr1/rec1'),
+            ('sort[0][direction]', 'asc'),
+            ('sort[0][field]', 'COLUMN_A'),
+            ('view', 'ViewName')
+        ])
+    )
     assert airtable_pages.session.request.call_count == 3
 
 
@@ -269,15 +398,55 @@ def test_get_iter(airtable_pages):
 
 
 @pytest.mark.update
-@pytest.mark.parametrize('fields', [
-    {'Status': 'Fired'}
-])
-def test_insert(fields, air_table):
+@pytest.mark.parametrize('fields', [{'Status': 'Fired'}])
+def test_update(fields, air_table):
     air_table.update('recwPQIfs4wKPyc9D', fields)
     air_table.session.request.assert_called_with(
         'patch',
         'https://api.airtable.com/v0/Base Key/Table%20Name/recwPQIfs4wKPyc9D',
-        json={'fields': {'Status': 'Fired'}, 'typecast': False}, params=None)
+        json={'fields': {'Status': 'Fired'}, 'typecast': False},
+        params=None
+    )
+    air_table.session.request.assert_called_once()
+
+
+@pytest.mark.update
+@pytest.mark.parametrize('field_name,field_value,fields,record_id', [
+    ('Name', 'Tom', {'Phone': '1234-4445'}, '2'),
+    ('Name', 'John',  {'Name': 'Johnny', 'Tel': '540-255-5522'}, '1'),
+    ('Name', 'Joe', {'Phone': '1234-4445'}, None),
+])
+def test_update_by_field(
+    field_name, field_value, fields, record_id, airtable_pages
+):
+    airtable_pages.match = Mock()
+    airtable_pages.match.side_effect = match
+    result = airtable_pages.update_by_field(field_name, field_value, fields)
+    assert airtable_pages.match.called_with(field_name, field_value)
+    assert airtable_pages.match.called_once()
+    if record_id:
+        airtable_pages.session.request.assert_called_with(
+            'patch',
+            'https://api.airtable.com/v0/Base Key/Table%20Name/' + record_id,
+            json={'fields': fields, 'typecast': False},
+            params=None
+        )
+        airtable_pages.session.request.assert_called_once()
+    else:
+        airtable_pages.session.request.assert_not_called()
+        assert result == {}
+
+
+@pytest.mark.update
+def test_replace(air_table):
+    fields = {'PassangerName': 'Mike', 'Passport': 'YASD232-23'}
+    air_table.replace('recwPQIfs4wKPyc9D', fields)
+    air_table.session.request.assert_called_with(
+        'put',
+        'https://api.airtable.com/v0/Base Key/Table%20Name/recwPQIfs4wKPyc9D',
+        json={'fields': fields, 'typecast': False},
+        params=None
+    )
     air_table.session.request.assert_called_once()
 
 
@@ -285,12 +454,14 @@ def test_insert(fields, air_table):
 
 
 @pytest.mark.delete
-def test_insert(air_table):
+def test_delete(air_table):
     air_table.delete('recwPQIfs4wKPyc9D')
     air_table.session.request.assert_called_with(
         'delete',
         'https://api.airtable.com/v0/Base Key/Table%20Name/recwPQIfs4wKPyc9D',
-        json=None, params=None)
+        json=None,
+        params=None
+    )
     air_table.session.request.assert_called_once()
 
 
@@ -301,30 +472,76 @@ def test_batch_delete(air_table):
     air_table.session.request.any_call(
         'delete',
         'https://api.airtable.com/v0/Base Key/Table%20Name/recwPQIfs4wKPyc9D',
-        json=None, params=None)
+        json=None,
+        params=None
+    )
     air_table.session.request.any_call(
         'delete',
         'https://api.airtable.com/v0/Base Key/Table%20Name/recwDxIfs3wDPyc3F',
-        json=None, params=None)
+        json=None,
+        params=None
+    )
+
+
+@pytest.mark.update
+@pytest.mark.parametrize(
+    'field_name,field_value,record_id', [
+        ('Name', 'Tom', '2'),
+        ('Name', 'John',  '1')
+    ]
+)
+def test_delete_by_field(field_name, field_value, record_id, airtable_pages):
+    # TODO: delete_by_field should behave the same as update_by_field
+    # when field/value match is not found.  update_by_field contains
+    # logic to handle this, delete_by_field does not
+    # once working add ('Name', 'Joe', None) to the parmaeters.
+    airtable_pages.match = Mock()
+    airtable_pages.match.side_effect = match
+    result = airtable_pages.delete_by_field(field_name, field_value)
+    assert airtable_pages.match.called_with(field_name, field_value)
+    assert airtable_pages.match.called_once()
+    if record_id:
+        airtable_pages.session.request.assert_called_with(
+            'delete',
+            'https://api.airtable.com/v0/Base Key/Table%20Name/' + record_id,
+            json=None,
+            params=None
+        )
+        airtable_pages.session.request.assert_called_once()
+    else:
+        airtable_pages.session.request.assert_not_called()
+        assert result == {}
 
 
 # --- SEARCH RECORDS ---
 
 
 @pytest.mark.search
-@pytest.mark.parametrize('args,params', [
-    (
-        ['Name', 'Tom'],
-        [('filterByFormula', "{Name}='Tom'"), ('offset', 'itr1/rec1')]
-    ), (
-        ['ColumnA', 'SearchValue'],
-        [('filterByFormula', "{ColumnA}='SearchValue'"),
-         ('offset', 'itr1/rec1')],
-    ), (
-        ['Gender', 'Male'],
-        [('filterByFormula', "{Gender}='Male'"), ('offset', 'itr1/rec1')]
-    )
-])
+@pytest.mark.parametrize(
+    'args,params', [
+        (
+            ['Name', 'Tom'],
+            [
+                ('filterByFormula', "{Name}='Tom'"),
+                ('offset', 'itr1/rec1')
+            ]
+        ),
+        (
+            ['ColumnA', 'SearchValue'],
+            [
+                ('filterByFormula', "{ColumnA}='SearchValue'"),
+                ('offset', 'itr1/rec1')
+            ],
+        ),
+        (
+            ['Gender', 'Male'],
+            [
+                ('filterByFormula',"{Gender}='Male'"),
+                ('offset', 'itr1/rec1')
+            ]
+        )
+    ]
+)
 def test_search(args, params, airtable_pages):
     records = airtable_pages.search(*args)
     airtable_pages.session.request.assert_called_with(
@@ -379,23 +596,10 @@ def test_no_match_found(air_table):
 TODO
 ====
 
-UPDATE
-------
-
-airtable.update_by_field('Name', 'Tom', {'Phone': '1234-4445'})
->>> record = {'Name': 'John', 'Tel': '540-255-5522'}
->>> airtable.update_by_field('Name', 'John', record)
-
->>> fields = {'PassangerName': 'Mike', 'Passport': 'YASD232-23'}
->>> airtable.replace(record['id'], fields)
-
 DELETE
 ------
 
 >>> record = airtable.delete_by_field('Employee Id', 'DD13332454')
 airtable.delete_by_field('Name', 'Tom')
-
->>> record_ids = ['recwPQIfs4wKPyc9D', 'recwDxIfs3wDPyc3F']
->>> airtable.batch_delete(records_ids)
 
 '''
