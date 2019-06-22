c0323f12c5b1a8d809496a12d6ab9db0cea4c12e
Brian Peterson <bpeterso2000@yahoo.com>
2019-06-22 01:24:01 -0400

    Fix Flake8 warnings

--- tests/test_request_errors.py
+++ tests/test_request_errors.py
@@ -1,21 +1,14 @@
-import sys
-
 import pytest
 from mock import Mock
 from requests import HTTPError
-from six.moves import reload_module as reload
-
-
-import airtable
 
 
 def http_error_with_url():
-        raise HTTPError('unable to process page%20url')
+    raise HTTPError("unable to process page%20url")
 
 
 def json_decoder_error():
     raise ValueError()
-
 
 
 def test_error_mesg_in_json(table, response):
@@ -26,14 +19,12 @@ def test_error_mesg_in_json(table, response):
     assert str(e).endswith("Not Found [Error: here's what went wrong]")
 
 
-
 def test_error_without_mesg_in_json(table, response):
     response.status_code = 404
     response.json = Mock(return_value={})
     with pytest.raises(HTTPError) as e:
         table._process_response(response)
-    assert str(e).endswith('Not Found')
-
+    assert str(e).endswith("Not Found")
 
 
 def test_404_error_with_json_decode_error(table, response):
