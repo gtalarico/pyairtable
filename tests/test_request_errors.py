e27a28e9dc3c5bb79191ec7764229881da7fe10e
Brian Peterson <bpeterso2000@yahoo.com>
2019-06-27 21:35:48 -0400

    Add coverage for JsonDecodeError

--- tests/test_request_errors.py
+++ tests/test_request_errors.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 from mock import Mock
 from requests import HTTPError
@@ -27,8 +29,8 @@ def test_error_without_mesg_in_json(table, response):
     assert str(e).endswith("Not Found")
 
 
-def test_404_error_with_json_decode_error(table, response):
-    response.status_code = 404
-    response.json.side_effect = json_decoder_error
-    with pytest.raises(ValueError):
+def test_non_422_error_with_json_decode_error(table, response):
+    response.status_code = 400
+    response.json.side_effect = json.decoder.JSONDecodeError('', '', 0)
+    with pytest.raises(HTTPError):
         table._process_response(response)
