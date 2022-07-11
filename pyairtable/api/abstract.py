3c50f1a23b9eaf90703a1e6c9cb024d54c5d16dc
Osvaldo Demo <osvaldo@tryfondo.com>
2022-07-11 16:17:41 -0300

    Add support for retry with exponential backoff

--- pyairtable/api/abstract.py
+++ pyairtable/api/abstract.py
@@ -1,12 +1,12 @@
 import abc
-from functools import lru_cache
 import posixpath
-from typing import List, Optional, Tuple
+import requests
 import time
+from functools import lru_cache
+from typing import List, Optional, Tuple
 from urllib.parse import quote
 
-import requests
-
+from pyairtable.utils import AutoRetrySession
 from .params import to_params_dict
 
 
@@ -17,7 +17,7 @@ class ApiAbstract(metaclass=abc.ABCMeta):
     API_URL = posixpath.join(API_BASE_URL, VERSION)
     MAX_RECORDS_PER_REQUEST = 10
 
-    session: requests.Session
+    session: AutoRetrySession
     tiemout: Optional[Tuple[int, int]]
 
     def __init__(self, api_key: str, timeout=None):
@@ -64,7 +64,7 @@ class ApiAbstract(metaclass=abc.ABCMeta):
     def _chunk(self, iterable, chunk_size):
         """Break iterable into chunks"""
         for i in range(0, len(iterable), chunk_size):
-            yield iterable[i : i + chunk_size]
+            yield iterable[i: i + chunk_size]
 
     def _build_batch_record_objects(self, records):
         return [{"fields": record} for record in records]
