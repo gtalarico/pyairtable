gtalarico/pyairtable

commit 0189d9d825168db0ff34bbd7cca1b263e3852564

Configurable URL for Airtable API endpoint (#243)

pyairtable/api/abstract.py
@@ -16,12 +16,11 @@ TimeoutTuple = Tuple[int, int]
 
 class ApiAbstract(metaclass=abc.ABCMeta):
     VERSION = "v0"
-    API_BASE_URL = "https://api.airtable.com/"
     API_LIMIT = 1.0 / 5  # 5 per second
-    API_URL = posixpath.join(API_BASE_URL, VERSION)
     MAX_RECORDS_PER_REQUEST = 10
 
     session: Session
+    endpoint_url: str
     tiemout: TimeoutTuple
 
     def __init__(
@@ -29,12 +28,14 @@ class ApiAbstract(metaclass=abc.ABCMeta):
         api_key: str,
         timeout: Optional[TimeoutTuple] = None,
         retry_strategy: Optional[Retry] = None,
+        endpoint_url: str = "https://api.airtable.com",
     ):
         if not retry_strategy:
             self.session = Session()
         else:
             self.session = _RetryingSession(retry_strategy)
 
+        self.endpoint_url = posixpath.join(endpoint_url, self.VERSION)
         self.timeout = timeout
         self.api_key = api_key
 
@@ -55,7 +56,7 @@ class ApiAbstract(metaclass=abc.ABCMeta):
     @lru_cache()
     def get_table_url(self, base_id: str, table_name: str):
         url_safe_table_name = quote(table_name, safe="")
-        table_url = posixpath.join(self.API_URL, base_id, url_safe_table_name)
+        table_url = posixpath.join(self.endpoint_url, base_id, url_safe_table_name)
         return table_url
 
     @lru_cache()

pyairtable/api/table.py
@@ -26,6 +26,7 @@ class Table(ApiAbstract):
         *,
         timeout: Optional[TimeoutTuple] = None,
         retry_strategy: Optional[Retry] = None,
+        endpoint_url: str = "https://api.airtable.com",
     ):
         """
         Args:
@@ -39,7 +40,13 @@ class Table(ApiAbstract):
         """
         self.base_id = base_id
         self.table_name = table_name
-        super().__init__(api_key, timeout=timeout, retry_strategy=retry_strategy)
+        self.endpoint_url = endpoint_url
+        super().__init__(
+            api_key,
+            timeout=timeout,
+            retry_strategy=retry_strategy,
+            endpoint_url=endpoint_url,
+        )
 
     @property
     def table_url(self):

pyairtable/metadata.py
@@ -29,7 +29,7 @@ def get_api_bases(api: Union[Api, Base]) -> dict:
                 ]
             }
     """
-    base_list_url = posixpath.join(api.API_URL, "meta", "bases")
+    base_list_url = posixpath.join(api.endpoint_url, "meta", "bases")
     return api._request("get", base_list_url)
 
 
@@ -78,7 +78,7 @@ def get_base_schema(base: Union[Base, Table]) -> dict:
             }
     """
     base_schema_url = posixpath.join(
-        base.API_URL, "meta", "bases", base.base_id, "tables"
+        base.endpoint_url, "meta", "bases", base.base_id, "tables"
     )
     return base._request("get", base_schema_url)
 

tests/test_api_api.py
@@ -20,6 +20,22 @@ def test_get_table(api: Api):
     assert rv.table_name == "y"
 
 
+def test_default_endpoint_url(api: Api):
+    assert api.endpoint_url == "https://api.airtable.com/" + api.VERSION
+
+
+def test_endpoint_url(api_with_endpoint_url: Api):
+    assert (
+        api_with_endpoint_url.endpoint_url
+        == "https://api.example.com/" + api_with_endpoint_url.VERSION
+    )
+
+
+def test_endpoint_url_with_trailing_slash():
+    api = Api("apikey", endpoint_url="https://api.example.com/")
+    assert api.endpoint_url == "https://api.example.com/" + api.VERSION
+
+
 @mock.patch.object(ApiAbstract, "_get_record")
 def test_get(m, api: Api, mock_response_single):
     m.return_value = mock_response_single

tests/test_api_table.py
@@ -21,7 +21,7 @@ def test_repr(table):
 )
 def test_url(base_id, table_name, table_url_suffix):
     table = Table("apikey", base_id, table_name)
-    assert table.table_url == "{0}/{1}".format(table.API_URL, table_url_suffix)
+    assert table.table_url == "{0}/{1}".format(table.endpoint_url, table_url_suffix)
 
 
 def test_chunk(table):

pyairtable/api/api.py
@@ -25,6 +25,7 @@ class Api(ApiAbstract):
         *,
         timeout: Optional[TimeoutTuple] = None,
         retry_strategy: Optional[Retry] = None,
+        endpoint_url: str = "https://api.airtable.com",
     ):
         """
 
@@ -34,23 +35,36 @@ class Api(ApiAbstract):
         Keyword Args:
             timeout (``Tuple``): |arg_timeout|
             retry_strategy (``Retry``): |arg_retry_strategy|
-
+            endpoint_url (``str``): |arg_endpoint_url|
         """
-        super().__init__(api_key, timeout=timeout, retry_strategy=retry_strategy)
+        super().__init__(
+            api_key,
+            timeout=timeout,
+            retry_strategy=retry_strategy,
+            endpoint_url=endpoint_url,
+        )
 
     def get_table(self, base_id: str, table_name: str) -> "Table":
         """
         Returns a new :class:`Table` instance using all shared
         attributes from :class:`Api`
         """
-        return Table(self.api_key, base_id, table_name, timeout=self.timeout)
+        return Table(
+            self.api_key,
+            base_id,
+            table_name,
+            timeout=self.timeout,
+            endpoint_url=self.endpoint_url,
+        )
 
     def get_base(self, base_id: str) -> "Base":
         """
         Returns a new :class:`Base` instance using all shared
         attributes from :class:`Api`
         """
-        return Base(self.api_key, base_id, timeout=self.timeout)
+        return Base(
+            self.api_key, base_id, timeout=self.timeout, endpoint_url=self.endpoint_url
+        )
 
     def get_record_url(self, base_id: str, table_name: str, record_id: str):
         """

pyairtable/api/base.py
@@ -23,6 +23,7 @@ class Base(ApiAbstract):
         *,
         timeout: Optional[TimeoutTuple] = None,
         retry_strategy: Optional[Retry] = None,
+        endpoint_url: str = "https://api.airtable.com",
     ):
         """
         Args:
@@ -35,7 +36,12 @@ class Base(ApiAbstract):
         """
 
         self.base_id = base_id
-        super().__init__(api_key, timeout=timeout, retry_strategy=retry_strategy)
+        super().__init__(
+            api_key,
+            timeout=timeout,
+            retry_strategy=retry_strategy,
+            endpoint_url=endpoint_url,
+        )
 
     def get_table(self, table_name: str) -> "Table":
         """

tests/conftest.py
@@ -15,7 +15,7 @@ def url_builder():
 
     def _url_builder(base_id, table_name, params=None):
         urltable_name = quote(table_name, safe="")
-        url = urljoin(Api.API_URL, base_id, urltable_name)
+        url = urljoin(Api.endpoint_url, base_id, urltable_name)
         if params:
             params = OrderedDict(sorted(params.items()))
             url += "?" + urlencode(params)
@@ -34,6 +34,11 @@ def constants():
 @pytest.fixture()
 def api(constants):
     return Api(constants["API_KEY"])
+
+
+@pytest.fixture()
+def api_with_endpoint_url(constants):
+    return Api(constants["API_KEY"], endpoint_url="https://api.example.com")
 
 
 @pytest.fixture()

commit 4c7b01bb7aa54d92c3eb0b1c56dc9a838fc4bc83

Fix regression introduced by `endpoint_url=`
As @goksan pointed out [here](https://github.com/gtalarico/pyairtable/pull/243#issuecomment-1523935537), there is untested behavior that regressed in my previous commit (0189d9d). When calling `api.get_base()` we need to ensure the version number does not get appended to the URL twice.

Rather than second-guessing what strings we receive in the constructor, I chose to implement `ApiAbstract.build_url()` that will ensure the correct version number gets inserted in between the endpoint_url and whatever URL components the caller needs to access.

pyairtable/api/abstract.py
@@ -35,7 +35,7 @@ class ApiAbstract(metaclass=abc.ABCMeta):
         else:
             self.session = _RetryingSession(retry_strategy)
 
-        self.endpoint_url = posixpath.join(endpoint_url, self.VERSION)
+        self.endpoint_url = endpoint_url
         self.timeout = timeout
         self.api_key = api_key
 
@@ -50,14 +50,20 @@ class ApiAbstract(metaclass=abc.ABCMeta):
         self._update_api_key(value)
         self._api_key = value
 
+    def build_url(self, *components: str) -> str:
+        """
+        Returns a URL to the Airtable API endpoint with the given URL components,
+        including the API version number.
+        """
+        return posixpath.join(self.endpoint_url, self.VERSION, *components)
+
     def _update_api_key(self, api_key: str) -> None:
         self.session.headers.update({"Authorization": "Bearer {}".format(api_key)})
 
     @lru_cache()
     def get_table_url(self, base_id: str, table_name: str):
         url_safe_table_name = quote(table_name, safe="")
-        table_url = posixpath.join(self.endpoint_url, base_id, url_safe_table_name)
-        return table_url
+        return self.build_url(base_id, url_safe_table_name)
 
     @lru_cache()
     def _get_record_url(self, base_id: str, table_name: str, record_id):

pyairtable/api/table.py
@@ -40,7 +40,6 @@ class Table(ApiAbstract):
         """
         self.base_id = base_id
         self.table_name = table_name
-        self.endpoint_url = endpoint_url
         super().__init__(
             api_key,
             timeout=timeout,

pyairtable/metadata.py
@@ -1,4 +1,3 @@
-import posixpath
 from typing import Optional, Union
 
 from pyairtable.api import Api, Base, Table
@@ -29,7 +28,7 @@ def get_api_bases(api: Union[Api, Base]) -> dict:
                 ]
             }
     """
-    base_list_url = posixpath.join(api.endpoint_url, "meta", "bases")
+    base_list_url = api.build_url("meta", "bases")
     return api._request("get", base_list_url)
 
 
@@ -77,9 +76,7 @@ def get_base_schema(base: Union[Base, Table]) -> dict:
                 ]
             }
     """
-    base_schema_url = posixpath.join(
-        base.endpoint_url, "meta", "bases", base.base_id, "tables"
-    )
+    base_schema_url = base.build_url("meta", "bases", base.base_id, "tables")
     return base._request("get", base_schema_url)
 
 

tests/test_api_api.py
@@ -1,6 +1,6 @@
 from unittest import mock
 
-from pyairtable import Api, Table
+from pyairtable import Api, Base, Table
 from pyairtable.api.abstract import ApiAbstract
 
 
@@ -13,27 +13,36 @@ def test_record_url(api: Api):
     assert rv == ApiAbstract("x")._get_record_url("baseid", "tablename", "rec")
 
 
+def test_get_base(api: Api):
+    rv = api.get_base("appTest")
+    assert isinstance(rv, Base)
+    assert rv.base_id == "appTest"
+    assert rv.endpoint_url == api.endpoint_url
+
+
 def test_get_table(api: Api):
-    rv = api.get_table("x", "y")
+    rv = api.get_table("appTest", "tblTest")
     assert isinstance(rv, Table)
-    assert rv.base_id == "x"
-    assert rv.table_name == "y"
+    assert rv.base_id == "appTest"
+    assert rv.table_name == "tblTest"
+    assert rv.endpoint_url == api.endpoint_url
+    assert rv.table_url == "https://api.airtable.com/v0/appTest/tblTest"
 
 
 def test_default_endpoint_url(api: Api):
-    assert api.endpoint_url == "https://api.airtable.com/" + api.VERSION
+    rv = api.build_url("appTest", "tblTest")
+    assert rv == "https://api.airtable.com/v0/appTest/tblTest"
 
 
 def test_endpoint_url(api_with_endpoint_url: Api):
-    assert (
-        api_with_endpoint_url.endpoint_url
-        == "https://api.example.com/" + api_with_endpoint_url.VERSION
-    )
+    rv = api_with_endpoint_url.build_url("appTest", "tblTest")
+    assert rv == "https://api.example.com/v0/appTest/tblTest"
 
 
 def test_endpoint_url_with_trailing_slash():
     api = Api("apikey", endpoint_url="https://api.example.com/")
-    assert api.endpoint_url == "https://api.example.com/" + api.VERSION
+    rv = api.build_url("appTest", "tblTest")
+    assert rv == "https://api.example.com/v0/appTest/tblTest"
 
 
 @mock.patch.object(ApiAbstract, "_get_record")

tests/test_api_base.py
@@ -10,7 +10,7 @@ def test_repr(base):
 
 def test_record_url(base: Base):
     rv = base.get_record_url("tablename", "rec")
-    assert rv == ApiAbstract("x")._get_record_url(base.base_id, "tablename", "rec")
+    assert rv == f"https://api.airtable.com/v0/{base.base_id}/tablename/rec"
 
 
 def test_get_table(base: Base):
@@ -18,6 +18,7 @@ def test_get_table(base: Base):
     assert isinstance(rv, Table)
     assert rv.base_id == base.base_id
     assert rv.table_name == "tablename"
+    assert rv.table_url == f"https://api.airtable.com/v0/{base.base_id}/tablename"
 
 
 @mock.patch.object(ApiAbstract, "_get_record")

tests/test_api_table.py
@@ -21,7 +21,7 @@ def test_repr(table):
 )
 def test_url(base_id, table_name, table_url_suffix):
     table = Table("apikey", base_id, table_name)
-    assert table.table_url == "{0}/{1}".format(table.endpoint_url, table_url_suffix)
+    assert table.table_url == f"https://api.airtable.com/v0/{table_url_suffix}"
 
 
 def test_chunk(table):