5d9ce9371b10cd3548bd230da0e52c6de2b7a4b8
Alex Levy <mesozoic@users.noreply.github.com>
2023-05-28 00:16:24 -0700

    Fix tests to pass with urllib3 2.0.0

--- tests/test_api_retrying.py
+++ tests/test_api_retrying.py
@@ -1,10 +1,14 @@
 """
-For these tests Mocker cannot be used because Retry is operating on a lower level
+For these tests Mocker cannot be used because Retry is operating on a lower level.
+Instead we use a real HTTP server running in a separate thread, which we can program
+to respond with various HTTP status codes.
 """
-import io
 import json
-from http.client import HTTPMessage, HTTPResponse
-from unittest import mock
+import os
+import threading
+from http import HTTPStatus
+from urllib.parse import urljoin
+from wsgiref.simple_server import make_server
 
 import pytest
 import requests
@@ -13,8 +17,87 @@ from pyairtable.api.retrying import retry_strategy
 from pyairtable.api.table import Table
 
 
+# Adapted from https://github.com/kevin1024/pytest-httpbin
+class Server:
+    """
+    HTTP server running a WSGI application in its own thread.
+    """
+
+    port_envvar = "HTTPBIN_HTTP_PORT"
+
+    def __init__(self, host="127.0.0.1", port=0, application=None, **kwargs):
+        self.app = application
+        if self.port_envvar in os.environ:
+            port = int(os.environ[self.port_envvar])
+        self._server = make_server(host, port, self.app, **kwargs)
+        self.host = self._server.server_address[0]
+        self.port = self._server.server_address[1]
+        self.protocol = "http"
+
+        self._thread = threading.Thread(
+            name=self.__class__,
+            target=self._server.serve_forever,
+        )
+
+    def __del__(self):
+        if hasattr(self, "_server"):
+            self.stop()
+
+    def start(self):
+        self._thread.start()
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self.stop()
+        suppress_exc = self._server.__exit__(*args, **kwargs)
+        self._thread.join()
+        return suppress_exc
+
+    def __add__(self, other):
+        return self.url + other
+
+    def stop(self):
+        self._server.shutdown()
+
+    @property
+    def url(self):
+        return f"{self.protocol}://{self.host}:{self.port}"
+
+    def join(self, url, allow_fragments=True):
+        return urljoin(self.url, url, allow_fragments=allow_fragments)
+
+
+class MockApi:
+    """
+    Dumb WSGI app that returns responses from a stack.
+    """
+
+    def __init__(self, responses=None):
+        self.responses = responses or []
+
+    def __call__(self, environ, start_response):
+        if not self.responses:
+            raise RuntimeError("MockApi.responses is empty")
+        status, jsondata = self.responses.pop(0)
+        if isinstance(status, int):
+            status = f"{status} {HTTPStatus(status).phrase}"
+        start_response(status, [("Content-Type", "application/json")])
+        if jsondata is None:
+            return []
+        return [json.dumps(jsondata).encode("utf8")]
+
+
+@pytest.fixture(scope="session")
+def mock_endpoint():
+    with Server(application=MockApi()) as server:
+        yield server
+
+
 @pytest.fixture
-def table_with_retry_strategy(constants):
+def table_with_retry_strategy(constants, mock_endpoint):
     def _table_with_retry(retry_strategy):
         return Table(
             constants["API_KEY"],
@@ -22,92 +105,51 @@ def table_with_retry_strategy(constants):
             constants["TABLE_NAME"],
             timeout=(0.1, 0.1),
             retry_strategy=retry_strategy,
+            endpoint_url=mock_endpoint.url,
         )
 
     return _table_with_retry
 
 
-@mock.patch("urllib3.connectionpool.HTTPConnectionPool._get_conn")
-def test_retry_exceed(m, table_with_retry_strategy):
+def test_retry_exceed(table_with_retry_strategy, mock_endpoint):
     strategy = retry_strategy(total=2, status_forcelist=[429])
     table = table_with_retry_strategy(strategy)
 
-    m.return_value.getresponse.side_effect = [
-        make_http_response_error(429),
-        make_http_response_error(429),
-        make_http_response_error(429),
-    ]
+    mock_endpoint.app.responses = [(429, None)] * 3
 
     with pytest.raises(requests.exceptions.RetryError):
         table.get("record")
 
-    assert len(m.return_value.request.mock_calls) == 3
 
-
-@mock.patch("urllib3.connectionpool.HTTPConnectionPool._get_conn")
-def test_retry_status_not_allowed(m, table_with_retry_strategy, mock_response_single):
+def test_retry_status_not_allowed(
+    table_with_retry_strategy,
+    mock_endpoint,
+    mock_response_single,
+):
     strategy = retry_strategy(total=2, status_forcelist=[429, 500])
     table = table_with_retry_strategy(strategy)
 
-    response = make_response(mock_response_single, 200)
-
-    m.return_value.getresponse.side_effect = [
-        make_http_response_error(401),
-        response,
+    mock_endpoint.app.responses = [
+        (401, None),
+        (200, mock_response_single),
     ]
 
     with pytest.raises(requests.exceptions.HTTPError):
-        response = table.get("record")
-
-    assert len(m.return_value.request.mock_calls) == 1
+        table.get("record")
 
 
-@mock.patch("urllib3.connectionpool.HTTPConnectionPool._get_conn")
-def test_retry_eventual_success(m, table_with_retry_strategy, mock_response_single):
+def test_retry_eventual_success(
+    table_with_retry_strategy,
+    mock_endpoint,
+    mock_response_single,
+):
     strategy = retry_strategy(total=2, status_forcelist=[429, 500])
     table = table_with_retry_strategy(strategy)
 
-    response = make_response(mock_response_single, 200)
-
-    m.return_value.getresponse.side_effect = [
-        make_http_response_error(429),
-        make_http_response_error(500),
-        response,
+    mock_endpoint.app.responses = [
+        (429, None),
+        (500, None),
+        (200, mock_response_single),
     ]
 
-    response = table.get("record")
-    assert response == mock_response_single
-    assert len(m.return_value.request.mock_calls) == 3
-
-
-# Test Helpers
-
-
-def make_response(body: dict, status=200) -> HTTPResponse:
-    headers = HTTPMessage()
-    body_bytes = json.dumps(body).encode()
-
-    sock = FakeSocketHelper(body_bytes)
-    response = HTTPResponse(sock)  # type: ignore
-    response.chunked = False  # type: ignore
-    response.length = len(body_bytes)  # type: ignore
-    response.status = status
-    response.msg = headers
-    return response
-
-
-def make_http_response_error(status: int):
-    return mock.Mock(status=status, msg=HTTPMessage())
-
-
-class FakeSocketHelper:
-    def __init__(self, text):
-        if isinstance(text, str):
-            text = text.encode("ascii")
-        self.text = text
-        self.data = b""
-        self.file_closed = False
-
-    def makefile(self, mode, bufsize=None):
-        self.file = io.BytesIO(self.text)
-        return self.file
+    assert table.get("record") == mock_response_single
