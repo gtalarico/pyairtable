592dabb385f02205b6e2bbf33b5c2da70ab65a6c
Benjamin Perkins <warriorben44@gmail.com>
2024-03-12 01:34:59 -0400

    Update `base.create_table`
    Turn off validation when creating a new table. See issue #343

--- pyairtable/api/base.py
+++ pyairtable/api/base.py
@@ -160,7 +160,7 @@ class Base:
         if description:
             payload["description"] = description
         response = self.api.post(url, json=payload)
-        return self.table(response["id"], validate=True)
+        return self.table(response["id"], validate=False)
 
     @property
     def url(self) -> str:
