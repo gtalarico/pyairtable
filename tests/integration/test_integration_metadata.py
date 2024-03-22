c035dc4afcf38b38a2fe2976dcc31b3d272b28e7
Alex Levy <mesozoic@users.noreply.github.com>
2024-03-22 11:00:20 -0700

    Remove deprecated `pyairtable.metadata` module

--- tests/integration/test_integration_metadata.py
+++ tests/integration/test_integration_metadata.py
@@ -1,8 +1,7 @@
 import pytest
 import requests
 
-from pyairtable import Api, Base, Table
-from pyairtable.metadata import get_api_bases, get_base_schema, get_table_schema
+from pyairtable import Api, Base
 
 pytestmark = [pytest.mark.integration]
 
@@ -37,32 +36,3 @@ def test_table_schema(base: Base, table_name: str, cols):
     assert cols.TEXT in [f.name for f in schema.fields]
     assert schema.field(cols.TEXT).id == cols.TEXT_ID
     assert schema.field(cols.TEXT_ID).name == cols.TEXT
-
-
-def test_deprecated_get_api_bases(base: Base, base_name: str):
-    with pytest.warns(DeprecationWarning):
-        rv = get_api_bases(base)
-    assert base_name in [b["name"] for b in rv["bases"]]
-
-
-def test_deprecated_get_base_schema(base: Base):
-    with pytest.warns(DeprecationWarning):
-        rv = get_base_schema(base)
-    assert sorted(table["name"] for table in rv["tables"]) == [
-        "Address",
-        "Contact",
-        "EVERYTHING",
-        "TEST_TABLE",
-    ]
-
-
-def test_deprecated_get_table_schema(table: Table):
-    with pytest.warns(DeprecationWarning):
-        rv = get_table_schema(table)
-    assert rv and rv["name"] == table.name
-
-
-def test_deprecated_get_table_schema__invalid_table(table, monkeypatch):
-    monkeypatch.setattr(table, "name", "DoesNotExist")
-    with pytest.warns(DeprecationWarning):
-        assert get_table_schema(table) is None
